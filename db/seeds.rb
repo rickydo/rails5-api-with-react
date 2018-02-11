5a2f27665ceb018effff46c62f5df53bf1bb5012
Ricky Do <ricky.dop@gmail.com>
2018-02-10 22:36:55 -0600

    fixed column name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ideas = Idea.create(
+  [
+    {
+      title: "Live at Homebase",
+      body: "They do awesome work"
+    },
+    {
+      title: "Work hard as you play",
+      body: "Why work so hard you never get to enjoy the fruits of your labor"
+    },
+    {
+      title: "Honeymoon in Italy",
+      body: "and we also went to Barcelona"
+    },
+    {
+      title: "Build an Api with React to catch Homebase's attention",
+      body: "Great Rails opportunity in Houston"
+    }
+  ])
