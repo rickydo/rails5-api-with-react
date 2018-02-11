32b2463e673c03a2b76ae2b9bb60c2a0a0d599bd
Ricky Do <ricky.dop@gmail.com>
2018-02-11 15:08:28 -0600

    add create endpoint on rails side to handle posts

--- rails /app/controllers/api/v1/ideas_controller.rb
+++ rails /app/controllers/api/v1/ideas_controller.rb
@@ -2,7 +2,17 @@ module Api::V1
   class IdeasController < ApplicationController
     def index
       @ideas = Idea.all
-      render json: @ideas 
+      render json: @ideas
+    end
+
+    def create
+      @idea = Idea.create(idea_params)
+      render json: @idea
+    end
+
+    private
+    def idea_params
+      params.require(:idea).permit(:title, :body)
     end
   end
 end
