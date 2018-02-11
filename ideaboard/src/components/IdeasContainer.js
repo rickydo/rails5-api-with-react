5559b6f8caee1384cbae239ea557be9735fb8d30
Ricky Do <ricky.dop@gmail.com>
2018-02-10 23:15:25 -0600

    adding axios

--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
 
 class IdeasContainer extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      ideas: []
+    }
+  }
+
+  componentDidMount(){
+
+  }
+
   render(){
     return (
       <div>
