699e5ccdbb32dbf7bb822bc3c908ac317e1df328
Ricky Do <ricky.dop@gmail.com>
2018-02-11 15:16:04 -0600

    react now posts with blank info

--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -22,6 +22,21 @@ class IdeasContainer extends Component {
       .catch(error => console.log(error))
   }
 
+  addNewIdea = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/ideas', {
+        idea: {
+          title: '',
+          body: ''
+        }
+      }
+    )
+    .then(res => {
+      console.log(res);
+    })
+    .catch( err => console.log(err))
+  }
+
   render(){
     return (
       <div>
@@ -30,7 +45,7 @@ class IdeasContainer extends Component {
             <Idea idea={idea} key={idea.id}/>
           )
         })}
-        <button className="newIdeaButton">
+        <button className="newIdeaButton" onClick={this.addNewIdea}>
           New Idea
         </button>
       </div>
